d244e59a9d3b745d7b352844835f96d7c34d2ea2
Kim Hung <kim.31@live.com.sg>
2021-03-30 22:15:28 +0800

    first commit

---
+++ ToDoList.Domain/Context/ToDoListContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoList.Domain.Models;
+
+namespace ToDoList.Domain.Context
+{
+    public class ToDoListContext : DbContext
+    {
+        public ToDoListContext()
+        {
+        }
+
+        public ToDoListContext(DbContextOptions<ToDoListContext> options) : base(options)
+        {
+        }
+
+        public DbSet<ToDoTask> ToDoTasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+           => options.UseSqlite(@"Data Source=C:\ToDoDatabase.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoTask>()
+                .ToTable("TO_DO_TASK");
+            modelBuilder.Entity<ToDoTask>()
+                .HasKey(x => x.TaskId);
+            modelBuilder.Entity<ToDoTask>()
+                .OwnsMany(x => x.SubTasks);
+            modelBuilder.Entity<ToDoTask>()
+                .HasQueryFilter(x => !x.IsDeleted);
+        }
+    }
+}
---
+++ ToDoList.Domain/Models/ToDoTask.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoList.Domain.Models
+{
+    public class ToDoTask
+    {
+        public int TaskId { get; set; }
+        public string TaskName { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset LastModifiedOn { get; set; }
+        public DateTimeOffset TargetCompletionDateTime { get; set; }
+        public string Description { get; set; }
+        public int ParentTaskLinkId { get; set; }
+        public List<ToDoTask> SubTasks { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsDeleted { get; set; }
+
+        protected ToDoTask()
+        {
+            CreatedOn = DateTimeOffset.Now;
+            LastModifiedOn = DateTimeOffset.Now;
+            SubTasks = new List<ToDoTask>();
+        }
+
+        public ToDoTask(string taskName, string description, DateTimeOffset targetCompletion, int parentId = 0) : base()
+        {
+            TaskName = taskName;
+            Description = description;
+            TargetCompletionDateTime = targetCompletion;
+            ParentTaskLinkId = parentId;
+        }
+
+        public void AddSubTask(string taskName, string description, DateTimeOffset targetCompletion)
+        {
+            SubTasks.Add(new ToDoTask(taskName, description, targetCompletion, TaskId));
+        }
+
+        public bool RemoveSubTask(ToDoTask task)
+        {
+            return SubTasks.Remove(task);
+        }
+
+        public void MarkAsCompleted()
+        {
+            IsCompleted = true;
+        }
+
+        public void MarkAsUncompleted()
+        {
+            IsCompleted = false;
+        }
+
+        public void EditDescription(string newDescription)
+        {
+            Description = newDescription;
+        }
+
+        public void UpdateTargetCompletionDate(DateTimeOffset newTarget)
+        {
+            TargetCompletionDateTime = newTarget;
+        }
+
+        public void DeleteTask()
+        {
+            IsDeleted = true;
+        }
+    }
+}
